d7120caed51c32574eb8866f652cc33a7db89f36
Samuel Lemos <107198491+Samuellbsilva@users.noreply.github.com>
2026-04-14 20:36:21 -0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,123 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite a capacidade máxima da sua lista de livros: ");
+        int tamanho = scanner.nextInt();
+
+
+
+        BookList minhaEbook = new BookList(tamanho);
+
+        int opcao;
+        do {
+
+
+            System.out.println("\n- MENU LISTA DE LIVROS -");
+            System.out.println("1. Adicionar livros ID");
+            System.out.println("2. Remover livros por ID");
+            System.out.println("3. Remover por Posição");
+            System.out.println("4. Buscar livros ID");
+            System.out.println("5. Atualizar ID de livros");
+            System.out.println("6. Ver Total de livros e Status");
+            System.out.println("7. Limpar lista de livros");
+            System.out.println("0. Sair");
+            System.out.println("Escolha uma opção: ");
+            opcao = scanner.nextInt();
+            System.out.println();
+
+            switch (opcao) {
+                case 1:
+                    if (minhaEbook.estaCheia()) {
+                        System.out.println("Erro: A lista de livros está lotada!");
+                    } else {
+                        System.out.println("Digite o ID do livro: ");
+                        int id = scanner.nextInt();
+
+                        minhaEbook.addBook(id);
+
+                        System.out.println();
+                        System.out.println("livro adicionado com sucesso.");
+                    }
+                    break;
+
+                case 2:
+                    System.out.println("Digite o ID para remover: ");
+                    int idRemover = scanner.nextInt();
+                    System.out.println();
+
+                    if (minhaEbook.removerBook(idRemover)) {
+                        System.out.println("livro  removido!");
+                    } else {
+
+                        System.out.println("livros não encontrado.");
+                    }
+                    break;
+
+                case 3:
+                    System.out.println("Digite a posição (0 a " + (minhaEbook.totalBook() - 1) + "): ");
+                    int pos = scanner.nextInt();
+                    System.out.println();
+
+                    if (minhaEbook.removerPorPosicao(pos)) {
+                        System.out.println("Posição removida!");
+                    } else {
+                        System.out.println("Posição inválida.");
+                    }
+                    break;
+
+                case 4:
+                    System.out.println("Digite o  ID para buscar: ");
+                    int idBusca = scanner.nextInt();
+                    System.out.println();
+
+                    if (minhaEbook.verificarExistencia(idBusca)) {
+                        int index = minhaEbook.obterBookPorPosicao(idBusca);
+
+
+                        System.out.println("livro encontrado na posição: " + index);
+                    } else {
+
+
+                        System.out.println("livros não encontrado.");
+                    }
+                    break;
+
+                case 5:
+                    System.out.println("ID antigo: ");
+                    int antigo = scanner.nextInt();
+
+
+                    System.out.println("Novo ID: ");
+                    int novo = scanner.nextInt();
+
+                    minhaEbook.atualizarID(antigo, novo);
+
+                    System.out.println();
+                    System.out.println("Processo  de atualização concluído.");
+                    break;
+
+                case 6:
+                    System.out.println(" Total de livros: " + minhaEbook.totalBook());
+                    System.out.println("Está cheia? " + (minhaEbook.estaCheia() ? "Sim" : "Não"));
+                    break;
+
+                case 7:
+                    minhaEbook.limparListaBook();
+                    System.out.println("list a de livros zerada.");
+                    break;
+
+                case 0:
+                    System.out.println("Saindo do sistema...");
+                    break;
+
+                default:
+                    System.out.println("Opção inválida! Tente novamente.");
+            }
+        } while (opcao != 0);
+
+        scanner.close();
+    }
+}
